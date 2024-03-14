bbce8496a30d89544394eabd06345e3b5f0dac51
Jesus Diaz <jdiazndca21@gmail.com>
2024-03-14 18:00:04 -0500

    Updated service and login component

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable} from "rxjs";
+import { Router, RouterOutlet } from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    private url = 'http://localhost:7074/api/Account';
+    private url = 'http://localhost:5268/api/Account';
 
-    constructor(private httpClient: HttpClient){}
+    constructor(private httpClient: HttpClient, private router: Router){}
 
-    async login(userEmail: string, password: string): Promise<any> {
+    async login(userEmail: string, password: string): Promise<Observable<any>> {
         try {
             const body = {userEmail, password}
-            const response = await this.httpClient.post<any>(`${this.url}/login`, body).toPromise();
+            const response = await this.httpClient.post<any>(`${this.url}/login/`, body);
+            this.router.navigate(['/home']);
             return response;
         }
         catch(error) {
